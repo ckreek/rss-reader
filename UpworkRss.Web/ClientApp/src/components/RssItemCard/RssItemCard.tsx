5ae20b43ee423d7263f72cab42d4f37820126f3b
Kirill Repeshko <repeshko.k@gmail.com>
2022-08-26 20:16:05 +0300

    added copy

--- UpworkRss.Web/ClientApp/src/components/RssItemCard/RssItemCard.tsx
+++ UpworkRss.Web/ClientApp/src/components/RssItemCard/RssItemCard.tsx
@@ -2,12 +2,14 @@ import Card from "@mui/material/Card";
 import CardContent from "@mui/material/CardContent";
 import Typography from "@mui/material/Typography";
 import { RssItem } from "store/RssItem/RssItemStore";
-import { useEffect, useRef } from "react";
+import { useEffect, useRef, useState } from "react";
 import { Box, Button } from "@mui/material";
 import { formatDate } from "utils";
 import { useRootStore } from "store/RootStore";
 import styles from "./RssItemCard.module.scss";
 import { observer } from "mobx-react-lite";
+import ContentCopyIcon from "@mui/icons-material/ContentCopy";
+import CheckIcon from '@mui/icons-material/Check';
 
 interface RssItemCardProps {
   item: RssItem;
@@ -16,6 +18,7 @@ interface RssItemCardProps {
 export const RssItemCard = observer(({ item }: RssItemCardProps) => {
   const { rssItemStore, feedStore } = useRootStore();
   const ref = useRef<HTMLSpanElement | null>(null);
+  const [copied, setCopied] = useState(false);
 
   useEffect(() => {
     if (ref.current) {
@@ -30,6 +33,19 @@ export const RssItemCard = observer(({ item }: RssItemCardProps) => {
   const handleReadClick = async () => {
     await rssItemStore.read(item);
   };
+
+  const copyUrlClick = async () => {
+    navigator.clipboard.writeText(item.url);
+    setCopied(true);
+  };
+
+  useEffect(() => {
+    const timeout = setTimeout(() => {
+      if (copied) setCopied(false);
+    }, 500);
+
+    return () => clearTimeout(timeout);
+  }, [copied]);
 
   return (
     <Card sx={{ width: "100%", position: "relative" }}>
@@ -98,6 +114,24 @@ export const RssItemCard = observer(({ item }: RssItemCardProps) => {
               onClick={handleHideClick}
             >
               Hide
+            </Button>
+            <Button
+              variant="outlined"
+              color="primary"
+              className={styles.button}
+              onClick={copyUrlClick}
+            >
+              Copy URL&nbsp;
+              <ContentCopyIcon
+                sx={{
+                  display: !copied ? "block" : "none",
+                }}
+              />
+              <CheckIcon
+                sx={{
+                  display: copied ? "block" : "none",
+                }}
+              />
             </Button>
           </Box>
         </Box>
