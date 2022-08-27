ed08d6e2548092eb9105454273a93dfd00fb6ad9
Kirill Repeshko <repeshko.k@gmail.com>
2022-08-27 14:20:05 +0300

    added router

--- UpworkRss.Web/ClientApp/src/App.tsx
+++ UpworkRss.Web/ClientApp/src/App.tsx
@@ -1,7 +1,14 @@
 import { Feeds } from "pages";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
 
 const App = () => {
-  return <Feeds />;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Feeds />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
